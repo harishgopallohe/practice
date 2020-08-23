afd5c55f448cc57c1ce89e34d0c453ac3417aabe
harishgopallohe <gopal_lohe@yahoo.co.in>
2020-08-23 14:33:33 +0530

    this is for practice

---
+++ src/test/java/practice1/test.java
@@ -0,0 +1,25 @@
+package practice1;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+	}
+
+}
